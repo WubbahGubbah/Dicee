eab679ada30c3e0578430763d9c5dc62ad0b1c26
Matthew Wilson <wubbahgubbah@gmail.com>
2018-01-20 09:26:05 -0500

    Start work with variables.
    Ensure no magic numbers, not mentioned in class.  Need to fix this screen.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var m_RandomDiceIndex1 : Int = 0
+    var m_RandomDiceIndex2 : Int = 0
+    var m_DiceRandomUpperBound : UInt32 = 6
+    
+    @IBOutlet weak var DiceImageView1: UIImageView!
+    @IBOutlet weak var DiceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func ButtonRollPressed(_ sender: UIButton) {
+        
+        m_RandomDiceIndex1 = Int(arc4random_uniform(m_DiceRandomUpperBound))
+        
+        m_RandomDiceIndex2 = Int(arc4random_uniform(m_DiceRandomUpperBound))
+        
+        print(m_RandomDiceIndex1)
+    }
+    
 }
 
